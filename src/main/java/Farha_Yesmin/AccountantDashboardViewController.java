cc81fe2e1cd3f6dbd470b2e5c3d58f2a7101723d
Farha Yesmin <farhayesmin.fa@gmail.com>
2024-12-15 06:06:44 +0600

    commit

--- src/main/java/Farha_Yesmin/AccountantDashboardViewController.java
+++ src/main/java/Farha_Yesmin/AccountantDashboardViewController.java
@@ -1,5 +1,6 @@
 package Farha_Yesmin;
 
+import Sauharda.CEOPlan;
 import com.sauharda.oop_projects.group_42.MainApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -26,6 +27,7 @@ public class AccountantDashboardViewController
 
     @javafx.fxml.FXML
     public void initialize() {
+        accountantDesignationLabel.setText("Accountant");
     }
 
     @javafx.fxml.FXML
@@ -144,7 +146,7 @@ public class AccountantDashboardViewController
     @javafx.fxml.FXML
     public void signOutOnAction(ActionEvent actionEvent) {
         try{
-            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
             Scene nextScene = new Scene(fxmlLoader.load());
             Stage currentStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
             currentStage.setTitle("Login Page");
@@ -180,9 +182,9 @@ public class AccountantDashboardViewController
             StringBuilder strBuilder = new StringBuilder();
             while (s.hasNextLine()) {
                 String str = s.nextLine();
-                //String[] tokens = str.split(",");
-                //CEOPlan c = new CEOPlan(tokens[0]);
-                //strBuilder.append(c.getGoal()).append("\n");
+                String[] tokens = str.split(",");
+                CEOPlan c = new CEOPlan(tokens[0]);
+                strBuilder.append(c.getGoal()).append("\n");
             }
             newGoalLabel.setText(strBuilder.toString());
         } catch (Exception e) {
